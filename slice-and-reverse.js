360babd4279034e187e0b7bfca638b0bab9aab6f
Christian Boyle <mail@christianboyle.com>
2021-04-04 09:19:17 -0500

    Add slice and reverse example

---
+++ slice-and-reverse.js
@@ -0,0 +1,10 @@
+// Method: Slice and reverse()
+// Explanation: Good, works with ES5
+
+let numbers = [1, 2, 3, 4, 5];
+
+let reversedNumbers = numbers.slice().reverse();
+
+console.log(reversedNumbers); // [5, 4, 3, 2, 1]
+
+console.log(numbers); // [1, 2, 3, 4, 5]
